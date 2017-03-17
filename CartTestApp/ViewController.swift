56d3ab143dcb8d5da316508414f6777d266836ec
Phil Wright <phillip.wright@eventboard.io>
2017-03-17 14:35:09 -0600

    Adding Carthage

--- CartTestApp/ViewController.swift
+++ CartTestApp/ViewController.swift
@@ -7,19 +7,16 @@
 //
 
 import UIKit
+import Alamofire
+import AlamofireImage
+import SwiftyJSON
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
+    
+    
 }
 
